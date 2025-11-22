dc17670abd4b319cdfe1a2793b08963e9c05213a
Saurabh-Bagchi <bagchi.saurabh@gmail.com>
2025-11-22 19:50:49 +0000

    first commit

--- week_2_mls/model_building/prep.py
+++ week_2_mls/model_building/prep.py
@@ -12,7 +12,7 @@ from huggingface_hub import login, HfApi
 
 # Define constants for the dataset and output paths
 api = HfApi(token=os.getenv("HF_TOKEN"))
-DATASET_PATH = "hf://datasets/praneeth232/machine-failure-prediction/machine-failure-prediction.csv"
+DATASET_PATH = "hf://datasets/SaurabhBagchi/machine-failure-prediction/machine-failure-prediction.csv"
 df = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
 
